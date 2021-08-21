b3ad7ff44d8e4bed9a97d4be2d5e413e95aee81a
Jacky <jackyhau97@gmail.com>
2021-08-21 13:50:01 +0700

    Fix bugs navbar on click.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,50 @@
-$('.page-scroll').on('click',function(e){
-   let tujuan = $(this).attr('href');         
-   let elemenTujuan = $(tujuan);         
-   console.log(elemenTujuan.offset().top);         
-   $('html').animate({            
-      scrollTop: elemenTujuan.offset().top - 50
-   }, '2000', 'easeInOutExpo');
-   e.preventDefault();         
+$(".page-scroll").on("click", function (e) {
+  let tujuan = $(this).attr("href");
+  let elemenTujuan = $(tujuan);
+  console.log(elemenTujuan.offset().top);
+  $("html").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 50,
+    },
+    "2000",
+    "easeInOutExpo"
+  );
+  e.preventDefault();
+  $("#bs-example-navbar-collapse-1").addClass("in");
+  $(".collapse").css("display", "none");
 });
 
 // about
-$(window).on('load',function(){
-   $('.pKiri').addClass('pMuncul');
-   $('.pKanan').addClass('pMuncul');
+$(window).on("load", function () {
+  $(".pKiri").addClass("pMuncul");
+  $(".pKanan").addClass("pMuncul");
 });
 // akhir about
 
-$(window).scroll(function(){   
-   let wScroll = $(this).scrollTop();
-   
-   // jumbotron
-   $('.jumbotron img').css({
-      'transform' : 'translate(0px, '+ wScroll/4 +'%)'
-   });
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
 
-   $('.jumbotron h1').css({
-      'transform' : 'translate(0px, '+ wScroll/2 +'%)'
-   });
+  // jumbotron
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 4 + "%)",
+  });
 
-   $('.jumbotron p').css({
-      'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
-   });
-   // akhir jumbotron
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)",
+  });
 
-   // portfolio
-   if (wScroll > $('.portfolio').offset().top - 250) {
-      $('.portfolio .group').each(function(i){         
-         setTimeout(function(){
-            $('.portfolio .group').eq(i).addClass('muncul');
-         }, 300 * i);      
-      });
-   }
-   // akhir portfolio
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 1.2 + "%)",
+  });
+  // akhir jumbotron
+
+  // portfolio
+  if (wScroll > $(".portfolio").offset().top - 250) {
+    $(".portfolio .group").each(function (i) {
+      setTimeout(function () {
+        $(".portfolio .group").eq(i).addClass("muncul");
+      }, 300 * i);
+    });
+  }
+  // akhir portfolio
 });
-
-// lightbox portfolio
-lightbox.option({
-   'alwaysShowNavOnTouchDevices' : true,
-   'disableScrolling' : true,
-   'fadeDuration' : 300,
-   'imageFadeDuration' : 300,
-   'positionFromTop' : 120,
-   'resizeDuration' : 300,
-   'wrapAround' : true
-});
-// akhir lightbox portfolio
